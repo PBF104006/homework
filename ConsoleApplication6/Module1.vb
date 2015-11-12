c7cee078d10dc05932d324c5758932220de002ec
pbf104006 <qaz10241324@gmai;.com>
2015-11-12 21:03:06 +0800

    pbf104006

---
+++ ConsoleApplication6/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim W, H As Integer
+        Console.Write("身高(公分) ")
+        H = Console.ReadLine
+        Console.Write("體重(公斤) ")
+        W = Console.ReadLine
+        Console.WriteLine("你的BMI值為" & (W / H ^ 2) * 10000)
+        Console.ReadLine()
+    End Sub
+
+End Module
